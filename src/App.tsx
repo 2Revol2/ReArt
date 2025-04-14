63bc0f37f2d56d52a01e6523ccb35ad2a0ee2e66
2Revol2 <maksimbaluta02@gmail.com>
2025-04-14 10:59:10 +0300

    feat: add react-router-dom and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Counter } from "./components/Counter";
+import { Link, Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 export const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О нас</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import { Counter } from "../../components/Counter";
+
+export default function MainPage() {
+  return (
+    <div>
+      <p>MainPage</p>
+      <Counter />
+    </div>
+  );
+}

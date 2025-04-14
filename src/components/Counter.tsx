693d2f39732bcef471904d8f6930f558b1ae24e6
2Revol2 <maksimbaluta02@gmail.com>
2025-04-14 10:27:10 +0300

    feat: add scss modules

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-import "./Counter.scss";
+import s from "./Counter.module.scss";
 
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
   return (
     <div>
       <h1> {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>increment</button>
-      <button onClick={() => setCounter(counter - 1)}>decrement</button>
+      <button className={s.button} onClick={() => setCounter(counter + 1)}>
+        increment
+      </button>
+      <button className={s.button} onClick={() => setCounter(counter - 1)}>
+        decrement
+      </button>
     </div>
   );
 };

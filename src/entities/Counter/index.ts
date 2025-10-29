1ee5c99035ddb0a9b9ee9b69a368de7084cb3a0a
2Revol2 <maksimbaluta02@gmail.com>
2025-10-29 12:29:49 +0100

    chore: update custom eslint plugin, add auto fix for public api imports

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,3 +1,4 @@
+export { Counter } from "./ui/Counter";
 export type { CounterSchema } from "./model/types/CounterSchema";
 export { CounterReducer } from "./model/slice/CounterSlice";
 export { CounterActions } from "./model/slice/CounterSlice";
